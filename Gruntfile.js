441f5e56564682dc08d62450e62589a1e4a80090
UltCombo <ultcombo@gmail.com>
2014-10-25 04:54:58 -0200

    Add `grunt watch` incremental build; Simplify Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,34 +5,32 @@ module.exports = function(grunt) {
 	require('load-grunt-tasks')(grunt);
 
 	grunt.initConfig({
-		clean: ['dist'],
+		clean: {
+			dist: 'dist',
+		},
 		'6to5': {
 			options: {
 			},
 			src: {
-				files: [{
-					expand: true,
-					cwd: 'src/',
-					src: ['**/*.js'],
-					dest: 'dist/',
-				}],
+				expand: true,
+				cwd: 'src',
+				src: '**/*.js',
+				dest: 'dist',
 			},
 		},
 		copy: {
 			src: {
-				files: [{
-					expand: true,
-					cwd: 'src/',
-					src: ['**/*', '!**/*.js'],
-					dest: 'dist/',
-				}],
+				expand: true,
+				cwd: 'src',
+				src: ['**', '!**/*.js'],
+				dest: 'dist',
 			},
 		},
 		jshint: {
 			options: {
 				jshintrc: true,
 			},
-			src: ['src/**/*.js'],
+			src: 'src/**/*.js',
 		},
 		jscs: {
 			options: {
@@ -40,17 +38,65 @@ module.exports = function(grunt) {
 				esnext: true,
 				reporter: 'inline',
 			},
-			src: ['src/**/*.js'],
+			src: 'src/**/*.js',
 		},
 		mochaTest: {
 			options: {
 				timeout: 5000,
+				reporter: 'nyan',
+				clearRequireCache: true, // necessary for `watch` task
 			},
-			src: ['dist/test/*.js'],
+			src: 'dist/test/*.js',
 		},
+		watch: {
+			options: {
+				spawn: false, // faster than `spawn: true`, also necessary for incremental build
+			},
+			// This Gruntfile auto-reload is buggy (tested in Windows)
+			// configFiles: {
+			// 	options: {
+			// 		reload: true,
+			// 	},
+			// 	files: ['Gruntfile.js', '.jshintrc', '.jscsrc'],
+			// },
+			js: {
+				options: {
+					event: ['added', 'changed'],
+				},
+				files: 'src/**/*.js',
+				tasks: ['6to5', 'test'],
+			},
+			copy: {
+				options: {
+					event: ['added', 'changed'],
+				},
+				files: ['src/**', '!src/**/*.js'],
+				tasks: ['copy', 'mochaTest'],
+			},
+			clean: {
+				options: {
+					event: 'deleted',
+				},
+				files: 'src/**',
+				tasks: ['clean', 'mochaTest'],
+			},
+		},
+	});
+
+	grunt.event.on('watch', function(action, filepath, target) {
+		filepath = filepath.replace(/\\/g, '/');
+		if (target === 'js') {
+			grunt.config('6to5.src.src', filepath.replace(/^src\//, ''));
+			grunt.config('jshint.src', filepath);
+			grunt.config('jscs.src', filepath);
+		} else if (target === 'copy') {
+			grunt.config('copy.src.src', filepath.replace(/^src\//, ''));
+		} else if (target === 'clean') {
+			grunt.config('clean.dist', filepath.replace(/^src/, 'dist'));
+		}
 	});
 
 	grunt.registerTask('build', ['clean', '6to5', 'copy']);
 	grunt.registerTask('test', ['jshint', 'jscs', 'mochaTest']);
-	grunt.registerTask('default', ['build', 'test']);
+	grunt.registerTask('default', ['build', 'test', 'watch']);
 };
