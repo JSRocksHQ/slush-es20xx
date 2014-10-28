1e3793e72350e887e3273ff949e8b3628ee091ae
UltCombo <ultcombo@gmail.com>
2014-10-28 08:52:27 -0200

    Add main slushfile; Readme: add developing section, fix npm casing

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # IN DEVELOPMENT - NOT READY FOR USE YET
 
 # [slush](https://github.com/slushjs/slush)-es6
-[![NPM version](http://img.shields.io/npm/v/slush-es6.svg)](https://npmjs.org/package/slush-es6)
+[![npm version](http://img.shields.io/npm/v/slush-es6.svg)](https://npmjs.org/package/slush-es6)
 [![Build Status](http://img.shields.io/travis/es6rocks/slush-es6.svg)](https://travis-ci.org/es6rocks/slush-es6)
 [![Dependency Status](http://img.shields.io/david/es6rocks/slush-es6.svg)](https://david-dm.org/es6rocks/slush-es6)
 [![devDependency Status](http://img.shields.io/david/dev/es6rocks/slush-es6.svg)](https://david-dm.org/es6rocks/slush-es6#info=devDependencies)
@@ -38,3 +38,13 @@ Then `cd` to the project root and run `npm run dev`. You're all set, happy hacki
 - `grunt build`: this task will build the `dist` directory based on the `src` directory's contents. More specifically, it will transpile all of the `src` directory's `.js` files and copy over all other files to `dist`, keeping the same directory structure.
 - `grunt test`: runs JSHint, JSCS and Mocha tests.
 - `grunt watch`: watches the `src` directory for changes, incrementally builds `dist` and runs the corresponding tests.
+
+# Developing
+
+Want to help developing slush-es6? Awesome! Here are the basic instructions to get you started:
+
+1. Fork this repository and clone it locally;
+- `cd` to your repository root and install the slush-es6 generator from your local repository by running `npm link`;
+- Create a test directory somewhere else, `cd` to it and run `slush es6` to execute this generator;
+- Edit the source code in your repository and then test the changes by running `slush es6` in a test directory. Repeat until you get the desired result;
+- Commit your changes to a new feature/bugfix branch, push them to your fork and open a PR in this repository. See [GitHub Flow](https://guides.github.com/introduction/flow/index.html) if you are not used to it yet. `=]`
---
+++ slushfile.js
@@ -0,0 +1,66 @@
+'use strict';
+
+var fs = require('fs'),
+	gulp = require('gulp'),
+	install = require('gulp-install'),
+	conflict = require('gulp-conflict'),
+	template = require('gulp-template'),
+	inquirer = require('inquirer'),
+	_ = require('lodash'),
+	chalk = require('chalk');
+
+function spaceSeparatedStrToArray(str) {
+	return str.trim().split(/\s+/);
+}
+
+gulp.task('default', function(done) {
+	inquirer.prompt([
+		{ name: 'env', message: 'Environment:', type: 'list', choices: [
+			{ value: 'node', name: 'Node.js' },
+			new inquirer.Separator('(more environments coming soon)')
+			// { value: 'browser', name: 'Browser' }
+		]},
+		{ when: _.createCallback({ env: 'node' }), name: 'pkgName', message: 'Package name:', default: 'my-es6-package' },
+		{ when: _.createCallback({ env: 'node' }), name: 'pkgDescription', message: 'Package description:', default: 'Hello ES6 World!' },
+		{ when: _.createCallback({ env: 'node' }), name: 'keywords', message: 'Keywords (space-separated):', default: 'es6 hello-world' },
+		{ when: _.createCallback({ env: 'node' }), name: 'installFlags', default: '--save', message: function(answers) {
+			return 'Recommended flag(s) for ' + chalk.bgGreen(' npm install ' + answers.pkgName + ' ');
+		}},
+		{ when: _.createCallback({ env: 'node' }), name: 'enginesNode', message: 'Compatible Node.js versions:', default: '>= 0.10' },
+		{ when: _.createCallback({ env: 'node' }), name: 'travisNode', message: 'Node.js versions to use in Travis CI (space-separated):', default: '0.10 0.11' },
+		{ when: _.createCallback({ env: 'node' }), name: 'authorName', message: 'Author name:', default: 'Ult Combo' },
+		{ when: _.createCallback({ env: 'node' }), name: 'authorEmail', message: 'Author email:', default: 'ultcombo@gmail.com' },
+		{ name: 'ghUser', message: 'GitHub repository owner username:', default: 'UltCombo' },
+		{ name: 'ghRepo', message: 'GitHub repository name:', default: function(answers) { return answers.pkgName; } },
+		{ name: 'homepage', message: 'Project homepage:', default: function(answers) { return 'https://github.com/' + answers.ghUser + '/' + answers.ghRepo; } },
+		{ name: 'license', message: 'License:', type: 'list', choices: fs.readdirSync(__dirname + '/tpl/licenses') },
+		{ type: 'confirm', name: 'moveon', message: 'Continue?' },
+	], function(answers) {
+		if (!answers.moveon) return done();
+
+		['keywords', 'travisNode'].forEach(function(prop) {
+			answers[prop] = spaceSeparatedStrToArray(answers[prop]);
+		});
+
+		answers.currentYear = new Date().getFullYear();
+
+		gulp.src([
+			__dirname + '/tpl/env/' + answers.env + '/**',
+			__dirname + '/tpl/licenses/' + answers.license + '/**'
+		], { dot: true })
+			// Setting the interpolate option in order to not conflict with ES6 template strings
+			.pipe(template(answers, { interpolate: /<%=([\s\S]+?)%>/g }))
+			.pipe(conflict('.'))
+			.pipe(gulp.dest('.'))
+			.pipe(install())
+			.on('end', function() {
+				console.log(chalk.green(
+					'All set!\n' +
+					'Run ' + chalk.white.bgGreen(' npm run dev ') + ' and hack the ' +
+					chalk.white.bgGreen(' src ') + ' directory!'
+				));
+				done();
+			})
+			.resume();
+	});
+});
