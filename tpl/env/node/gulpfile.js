de10487c6fca5219a932a78fa79b36b07575e8e9
UltCombo <ultcombo@gmail.com>
2014-12-22 01:47:47 -0200

    implement uniqueFilter, refactoring

--- tpl/env/node/gulpfile.js
+++ tpl/env/node/gulpfile.js
@@ -3,27 +3,23 @@
 var path = require('path'),
 	gulp = require('gulp'),
 	plugins = require('gulp-load-plugins')(),
+	_ = require('lodash'),
 	rimraf = require('rimraf'),
 	through = require('through'),
 	mergeStream = require('merge-stream'),
 	lazypipe = require('lazypipe'),
 	chalk = require('chalk'),
 	build = require('./build.json'),
-	handleJs = lazypipe()
+	copySrc = ['**'].concat(negateGlobs(build.src.js)),
+	writePipe = lazypipe()
+		.pipe(gulp.dest, build.distBase),
+	jsPipe = lazypipe()
 		.pipe(plugins.jshint)
 		.pipe(plugins.jshint.reporter, 'jshint-stylish')
 		.pipe(plugins.jshint.reporter, 'fail')
 		.pipe(plugins.jscs, { configPath: '.jscsrc', esnext: true })
 		.pipe(plugins['6to5']/*, { blacklist: ['generators'] }*/)
-		.pipe(gulp.dest, build.distBase),
-	handleCopy = lazypipe()
-		.pipe(gulp.dest, build.distBase),
-	handleDeletion = lazypipe()
-		.pipe(plugins.rename, function(filePath) {
-			// we can't change/remove filePath's `base`, so cd out of it in the dirname
-			filePath.dirname = path.join(path.relative(build.srcBase, '.'), build.distBase, filePath.dirname);
-		})
-		.pipe(plugins.rimraf),
+		.pipe(writePipe),
 	runTests = lazypipe()
 		.pipe(gulp.src, build.distBase + 'test/*.js', { read: false })
 		.pipe(plugins.mocha/*, { bail: true, timeout: 5000 }*/);
@@ -54,11 +50,9 @@ function runAfterEnd(cb) {
 
 gulp.task('build', function() {
 	rimraf.sync(build.distBase);
-	var jsSrc = prefixGlobs(build.src.js, build.srcBase),
-		copySrc = [build.srcBase + '**'].concat(negateGlobs(jsSrc));
 	return mergeStream(
-			gulp.src(jsSrc, { base: build.srcBase }).pipe(handleJs()),
-			gulp.src(copySrc, { base: build.srcBase }).pipe(handleCopy())
+			gulp.src(prefixGlobs(build.src.js, build.srcBase), { base: build.srcBase }).pipe(jsPipe()),
+			gulp.src(prefixGlobs(copySrc, build.srcBase), { base: build.srcBase }).pipe(writePipe())
 		)
 		.pipe(runAfterEnd(runTests));
 });
@@ -66,28 +60,45 @@ gulp.task('build', function() {
 // `neverEnd` receives a task conclusion callback which is never called as to signal that this watch task should never end.
 // We don't return gulp-watch's endless stream as it would fail the task in the first stream error.
 gulp.task('default', ['build'], function(neverEnd) {
+	var handleJs = lazypipe()
+			.pipe(plugins.filter, build.src.js)
+			.pipe(jsPipe),
+		handleCopy = lazypipe()
+			.pipe(plugins.filter, copySrc)
+			.pipe(writePipe),
+		handleDeletion = lazypipe()
+			.pipe(plugins.filter, filterEvent.bind(null, ['deleted']))
+			.pipe(plugins.rename, function(filePath) {
+				// we can't change/remove filePath's `base`, so cd out of it in the dirname
+				filePath.dirname = path.join(path.relative(build.srcBase, '.'), build.distBase, filePath.dirname);
+			})
+			.pipe(plugins.rimraf),
+		uniqueFilter = lazypipe()
+			.pipe(function() {
+				var files = [];
+				return through(function(file) {
+					var idx = _.findIndex(files, { path: file.path });
+					files[~idx ? idx : files.length] = file;
+				}, function() {
+					files.forEach(this.queue, this);
+					this.queue(null);
+				});
+			}),
+		existsFilter = lazypipe()
+			.pipe(plugins.filter, filterEvent.bind(null, ['changed', 'added']));
+
 	function filterEvent(events, file) {
 		return ~events.indexOf(file.event);
 	}
 
 	plugins.watch(build.srcBase + '**', { base: build.srcBase }, plugins.batch(function(files) {
-		// TODO filter buffers with repeated file paths
-		// TODO optimize
-		var jsFilter = plugins.filter(build.src.js),
-			copyFilter = plugins.filter(['**'].concat(negateGlobs(build.src.js))),
-			existsFilter = plugins.filter(filterEvent.bind(null, ['changed', 'added'])),
-			deletedFilter = plugins.filter(filterEvent.bind(null, ['deleted'])),
-			existsStream = files.pipe(existsFilter);
+		files = files.pipe(uniqueFilter());
+		var existingFiles = files.pipe(existsFilter());
 
 		return mergeStream(
-			existsStream
-				.pipe(jsFilter)
-				.pipe(handleJs()),
-			existsStream
-				.pipe(copyFilter)
-				.pipe(handleCopy()),
-			files.pipe(deletedFilter)
-				.pipe(handleDeletion())
+			existingFiles.pipe(handleJs()),
+			existingFiles.pipe(handleCopy()),
+			files.pipe(handleDeletion())
 		)
 		.pipe(runAfterEnd(runTests));
 	})).on('ready', function() {
