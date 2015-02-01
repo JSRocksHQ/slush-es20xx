10e9bf4b6920ef28b64c00ea800866328aae92b6
UltCombo <ultcombo@gmail.com>
2015-02-01 00:17:54 -0200

    Work around npm renaming .gitignore to .npmignore, resolves #7 and works around npm/npm#7252

---
+++ install.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var fs = require('fs'),
+	glob = require('glob');
+
+new glob.Glob('tpl/**/.npmignore')
+	.on('match', function(path) {
+		fs.rename(path, path.replace(/\.npmignore$/, '.gitignore'), function(err) {
+			if (err) throw err;
+		});
+	})
+	.on('error', function(err) {
+		throw err;
+	});
