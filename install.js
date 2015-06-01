c31ffdea247b2fb4ea0ddf62276a21794c39ada4
UltCombo <ultcombo@gmail.com>
2015-05-31 21:02:16 -0300

    reindent, code style

--- install.js
+++ install.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var fs = require('fs'),
-	glob = require('glob');
+var fs = require('fs');
+var glob = require('glob');
 
 new glob.Glob('tpl/**/.npmignore')
-	.on('match', function(path) {
-		fs.rename(path, path.replace(/\.npmignore$/, '.gitignore'), function(err) {
-			if (err) throw err;
-		});
-	})
-	.on('error', function(err) {
-		throw err;
-	});
+    .on('match', function(path) {
+        fs.rename(path, path.replace(/\.npmignore$/, '.gitignore'), function(err) {
+            if (err) throw err;
+        });
+    })
+    .on('error', function(err) {
+        throw err;
+    });
