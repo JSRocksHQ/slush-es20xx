9c824525ea20ef114baf96ce9db3d8caf013fbf2
UltCombo <ultcombo@gmail.com>
2015-07-04 03:48:48 -0300

    code style
    Adapt to project's reality.

--- install.js
+++ install.js
@@ -4,11 +4,11 @@ var fs = require('fs');
 var glob = require('glob');
 
 new glob.Glob('tpl/**/.npmignore')
-    .on('match', function(path) {
-        fs.rename(path, path.replace(/\.npmignore$/, '.gitignore'), function(err) {
-            if (err) throw err;
-        });
-    })
-    .on('error', function(err) {
-        throw err;
-    });
+	.on('match', function(path) {
+		fs.rename(path, path.replace(/\.npmignore$/, '.gitignore'), function(err) {
+			if (err) throw err;
+		});
+	})
+	.on('error', function(err) {
+		throw err;
+	});
